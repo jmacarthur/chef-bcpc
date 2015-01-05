492a7178729ff562f73cc236d88b3e62d5fb0924
Jim MacArthur <jim.macarthur@codethink.co.uk>
2015-01-05 16:41:43 +0000

    Partial revert to bc658e5: ceph-disk-activate does not take --cluster

--- cookbooks/bcpc/recipes/ceph-work.rb
+++ cookbooks/bcpc/recipes/ceph-work.rb
@@ -47,7 +47,7 @@ end
         execute "ceph-disk-prepare-#{type}-#{disk}" do
             command <<-EOH
                 ceph-disk-prepare /dev/#{disk} --cluster ceph
-                ceph-disk-activate /dev/#{disk} --cluster ceph
+                ceph-disk-activate /dev/#{disk}
                 sleep 2
                 INFO=`df -k | grep /dev/#{disk} | awk '{print $2,$6}' | sed -e 's/\\/var\\/lib\\/ceph\\/osd\\/ceph-//'`
                 OSD=${INFO#* }
